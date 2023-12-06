a1703fe0d76f1394146c90bf2ff04289fca888cc
Rhys <rhys.powell@gmail.com>
2023-12-06 16:14:51 +0000

    clean up script working for current build

--- destroy-script.sh
+++ destroy-script.sh
@@ -1,16 +1,14 @@
 export AWS_REGION=eu-west-1
 
-cluster_details=`rosa describe cluster -c rhys-hcp -o json`
-cluster_id=`cat $cluster_details | jq .id`
-operator_role_prefix=`cat $cluster_details | jq .aws.sts.operator_iam_roles.operator_role_prefix`
+oidc_config_id=`rosa describe cluster -c rhys-hcp -o json | jq -r .aws.sts.oidc_config.id`
 
 rosa delete cluster -c rhys-hcp -y
 echo "watch cluster uninstall"
 rosa logs uninstall -c rhys-hcp --watch
 
-rosa delete oidc-provider --oidc-config-id $cluster_id --mode auto -y
-rosa delete operator-roles --cluster rhys-hcp --mode auto -y
-rosa delete account-roles --hosted-cp --mode auto --prefix rhys-hcp --yes
-rosa delete oidc-config --oidc-config-id $cluster_id --mode auto -y
 
-terraform destroy -y
+rosa delete operator-roles --prefix rhys-hcp --mode auto -y
+rosa delete account-roles --hosted-cp --mode auto --prefix rhys-hcp -y
+rosa delete oidc-provider --oidc-config-id $oidc_config_id --mode auto -y
+
+terraform destroy -auto-approve
