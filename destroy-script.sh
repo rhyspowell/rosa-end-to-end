0d4ef709c6edacbf841f88c956ef3434d910c96e
Rhys <rhys.powell@gmail.com>
2024-07-29 14:20:58 +0100

    clean destroy script

--- destroy-script.sh
+++ destroy-script.sh
@@ -1,14 +1,25 @@
-export AWS_REGION=eu-west-1
+export AWS_REGION=eu-west-2
+CLUSTER_NAME="rhys-hcp"
+while getopts c: flag
+do
+    case "${flag}" in
+        c) CLUSTER_NAME=${OPTARG};;
+    esac
+done
 
-oidc_config_id=`rosa describe cluster -c rhys-hcp -o json | jq -r .aws.sts.oidc_config.id`
+# oidc_config_id=`rosa describe cluster -c rhys-hcp -o json | jq -r .aws.sts.oidc_config.id`
+oidc_config_id=`cat oidc_config_id.txt`
 
-rosa delete cluster -c rhys-hcp -y
+rosa delete cluster -c $CLUSTER_NAME -y
 echo "watch cluster uninstall"
-rosa logs uninstall -c rhys-hcp --watch
+rosa logs uninstall -c $CLUSTER_NAME --watch
 
 
-rosa delete operator-roles --prefix rhys-hcp --mode auto -y
-rosa delete account-roles --hosted-cp --mode auto --prefix rhys-hcp -y
+rosa delete operator-roles --prefix $CLUSTER_NAME --mode auto -y
+rosa delete account-roles --hosted-cp --mode auto --prefix $CLUSTER_NAME -y
 rosa delete oidc-provider --oidc-config-id $oidc_config_id --mode auto -y
+rosa delete oidc-config --oidc-config-id $oidc_config_id --mode auto -y
+
+rm oidc_config_id.txt
 
 terraform destroy -auto-approve
