e8856c3cf9b4284f77573c7a65c0545b57ba59c5
Rhys <rhys.powell@gmail.com>
2025-05-08 11:59:17 +0100

    update default variables so subnets will still get created

--- variables.tf
+++ variables.tf
@@ -24,13 +24,13 @@ variable "availability_zones" {
 variable "private_subnet_cidrs" {
   type        = list(any)
   description = "VPC privatge subnets"
-  default     = []
+  default     = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "public_subnet_cidrs" {
   type        = list(any)
   description = "VPC public subnets"
-  default     = []
+  default     = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
 }
 variable "single_nat_gateway" {
   type        = bool
